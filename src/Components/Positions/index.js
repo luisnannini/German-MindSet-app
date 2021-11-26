cdebba0d167fc26813f54268494c5ff1ac85f573
nicolasblasco <nicoblasco96@gmail.com>
2021-11-26 14:51:47 -0300

    MS-06: Import list component to Postions index.js

--- src/Components/Positions/index.js
+++ src/Components/Positions/index.js
@@ -1,9 +1,11 @@
 import styles from './positions.module.css';
+import List from './List';
 
 function Positions() {
   return (
     <section className={styles.container}>
       <h2>Positions</h2>
+      <List />
     </section>
   );
 }
