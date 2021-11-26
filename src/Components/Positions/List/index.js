7ee5f3c6481788574c99e60622ab1e2bfee31f82
nicolasblasco <nicoblasco96@gmail.com>
2021-11-26 14:49:31 -0300

    MS-06: Create list component

---
+++ src/Components/Positions/List/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const List = () => {
+  return (
+    <ul>
+      <li>ID</li>
+      <li>Client</li>
+      <li>Professional Profile</li>
+      <li>jobDescription</li>
+      <li>Vacancy</li>
+      <li>Is Open</li>
+    </ul>
+  );
+};
+
+export default List;
