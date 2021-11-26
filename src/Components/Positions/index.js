74b832d0640600c70592aeb99fff8ea8af724e7c
nicolasblasco <nicoblasco96@gmail.com>
2021-11-26 19:30:25 -0300

    MS-06: Add Positions function to retrieve all positions & Add useState & useEffect from react

--- src/Components/Positions/index.js
+++ src/Components/Positions/index.js
@@ -1,7 +1,17 @@
+import { useState, useEffect } from 'react';
 import styles from './positions.module.css';
 import List from './List';
 
 function Positions() {
+  const [positions, setPositions] = useState([]);
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API}/positions`)
+      .then((response) => response.json())
+      .then((response) => {
+        setPositions(response.data);
+      });
+  }, []);
+
   return (
     <section className={styles.container}>
       <h2>Positions</h2>
