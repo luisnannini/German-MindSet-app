046ec05653ecda4b95a9c7c64799a6a37a70de82
nicolasblasco <nicoblasco96@gmail.com>
2021-11-27 15:25:23 -0300

    MS-06: Add create button component

---
+++ src/Components/Positions/CreateButton/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import create from './create.png';
+
+const CreateButton = () => {
+  return <img src={create}></img>;
+};
+
+export default CreateButton;
--- src/Components/Positions/index.js
+++ src/Components/Positions/index.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import styles from './positions.module.css';
 import List from './List';
+import CreateButton from './CreateButton';
 // import Modal from './Modal';
 
 function Positions() {
@@ -13,33 +14,38 @@ function Positions() {
       });
   }, []);
   return (
-    <section className={styles.container}>
-      <h2>Positions</h2>
-      <List
-        client={'Client'}
-        profiles={'Professional Profiles'}
-        jobDescription={'Job Description'}
-        vacancy={'Vacancy'}
-        isOpen={'Is Open'}
-        update={''}
-        delete={''}
-      />
-      <div>
-        {positions.map((position) => {
-          return (
-            <List
-              client={position.client.name}
-              profiles={position.professionalProfiles}
-              jobDescription={position.jobDescription}
-              vacancy={position.vacancy}
-              isOpen={position.isOpen ? 'Yes' : 'No'}
-              update={''}
-              delete={''}
-            />
-          );
-        })}
+    <section>
+      <div className={styles.list}>
+        <h2>Positions</h2>
+        <List
+          client={'Client'}
+          profiles={'Professional Profiles'}
+          jobDescription={'Job Description'}
+          vacancy={'Vacancy'}
+          isOpen={'Is Open'}
+          update={''}
+          delete={''}
+        />
+        <div>
+          {positions.map((position) => {
+            return (
+              <List
+                client={position.client.name}
+                profiles={position.professionalProfiles}
+                jobDescription={position.jobDescription}
+                vacancy={position.vacancy}
+                isOpen={position.isOpen ? 'Yes' : 'No'}
+                update={''}
+                delete={''}
+              />
+            );
+          })}
+        </div>
+        {/* <Modal /> */}
       </div>
-      {/* <Modal /> */}
+      <div className={styles.button}>
+        <CreateButton />
+      </div>
     </section>
   );
 }
