8ee22ff8cc6e2de63623e6ee2e4dd4f6d3780441
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-27 13:08:18 -0300

    MS-05: Add GET request & create a list to bring all the intweviews

--- src/Components/Interviews/index.js
+++ src/Components/Interviews/index.js
@@ -1,9 +1,49 @@
+import React, { useEffect, useState } from 'react';
 import styles from './interviews.module.css';
+import List from './List';
 
 function Interviews() {
+  const [interviews, setInterviews] = useState([]);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API}/interviews`)
+      .then((response) => response.json())
+      .then((response) => {
+        setInterviews(response.data);
+      });
+  }, []);
+
   return (
     <section className={styles.container}>
       <h2>Interviews</h2>
+      <ul className={styles.list1}>
+        <li>Postulant</li>
+        <li>Client</li>
+      </ul>
+      <ul className={styles.list2}>
+        <li>First Name</li>
+        <li>Last Name</li>
+        <li>Name</li>
+        <li>Status</li>
+        <li>Date</li>
+        <li>Notes</li>
+        <li>Actions</li>
+      </ul>
+      <div>
+        {interviews.map((interview) => {
+          return (
+            <List
+              key={interview._id}
+              postulant={interview.postulant}
+              client={interview.client}
+              // application={interview.application}
+              status={interview.status}
+              date={interview.date}
+              notes={interview.notes}
+            />
+          );
+        })}
+      </div>
     </section>
   );
 }
