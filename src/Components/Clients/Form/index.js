b5a3c8a13f869750a6f16621a5f6e21752ef279d
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-11-28 12:41:30 -0300

    MS-04: form component modified and some styling added

--- src/Components/Clients/Form/index.js
+++ src/Components/Clients/Form/index.js
@@ -1,85 +1,158 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './form.module.css';
+import Input from '../Input';
 
-const Form = () => {
+const Form = (props) => {
+  const [nameValue, setNameValue] = useState('');
+  const [phoneValue, setPhoneValue] = useState('');
+  const [countryValue, setCountryValue] = useState('');
+  const [stateValue, setStateValue] = useState('');
+  const [cityValue, setCityValue] = useState('');
+  const [addressValue, setAddressValue] = useState('');
+  const [logoValue, setLogoValue] = useState('');
+  const [descriptionValue, setDescriptionValue] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: nameValue,
+        phone: phoneValue,
+        country: countryValue,
+        state: stateValue,
+        city: cityValue,
+        address: addressValue,
+        logo: logoValue,
+        description: descriptionValue
+      })
+    };
+
+    const url = `${process.env.REACT_APP_API}/clients`;
+
+    fetch(url, options).then((response) => {
+      if (response.status !== 200 && response.status !== 201) {
+        return response.json().then(({ message }) => {
+          throw new Error(message);
+        });
+      }
+      return response.json();
+    });
+  };
+
   return (
-    <div className={styles.container}>
-      <h2>Form</h2>
-      <form>
-        <div>
-          <input
-            id="name"
-            name="name"
-            placeholder="Client name"
-            type="text"
-            required
-            pattern="[A-Za-z ]*"
-            title="Enter a valid name"
-          />
-        </div>
-        <span className={styles.hiddenError}>Invalid name</span>
-        <div>
-          <input
-            id="phone"
-            name="phone"
-            placeholder="Client phone"
-            type="phone"
-            required
-            pattern="^[0-9,$]"
-            title="Enter a valid phone number"
-          />
-        </div>
-        <span className={styles.hiddenError}>Invalid phone number</span>
-        <div>
-          <input
-            id="country"
-            name="country"
-            placeholder="Client country"
-            type="text"
-            required
-            pattern="[A-Za-z ]*"
-            title="Enter a valid country"
-          />
-        </div>
-        <span className={styles.hiddenError}>Invalid country</span>
-        <div>
-          <input
-            id="state"
-            name="state"
-            placeholder="Client state"
-            type="text"
-            pattern="[A-Za-z ]*"
-            title="Enter a valid state"
-            required
-          />
-        </div>
-        <span className={styles.hiddenError}>Invalid state</span>
-        <div>
-          <input
-            id="city"
-            name="city"
-            placeholder="Client city"
-            type="text"
-            required
-            pattern="[A-Za-z ]*"
-            title="Enter a valid city"
-          />
-        </div>
-        <span className={styles.hiddenError}>Invalid city</span>
-        <div>
-          <input
-            id="address"
-            name="address"
-            placeholder="Client address"
-            type="address"
-            required
-            pattern="^[0-9,$], [A-Za-z ]"
-            title="Enter a valid address"
-          />
-        </div>
-        <span className={styles.hiddenError}>Invalid address</span>
-      </form>
-    </div>
+    <form className={styles.form} onSubmit={onSubmit}>
+      <h2>Clients Form</h2>
+      <div className={styles.formContainer}>
+        <label>Name</label>
+        <Input
+          name="name"
+          value={nameValue}
+          placeholder="Client name"
+          onChange={(e) => {
+            setNameValue(e.target.value);
+          }}
+          type="text"
+          required
+          pattern="[A-Za-z ]*"
+          title="Enter a valid name"
+        />
+        <label>Phone Number</label>
+        <Input
+          name="phone"
+          value={phoneValue}
+          placeholder="Client phone"
+          onChange={(e) => {
+            setPhoneValue(e.target.value);
+          }}
+          type="phone"
+          required
+          pattern="^[0-9,$]"
+          title="Enter a valid phone number"
+        />
+        <label>Country</label>
+        <Input
+          name="country"
+          value={countryValue}
+          placeholder="Client country"
+          onChange={(e) => {
+            setCountryValue(e.target.value);
+          }}
+          type="text"
+          required
+          pattern="[A-Za-z ]*"
+          title="Enter a valid country"
+        />
+        <label>State</label>
+        <Input
+          name="state"
+          value={stateValue}
+          placeholder="Client state"
+          onChange={(e) => {
+            setStateValue(e.target.value);
+          }}
+          type="text"
+          pattern="[A-Za-z ]*"
+          title="Enter a valid state"
+          required
+        />
+        <label>City</label>
+        <Input
+          name="city"
+          value={cityValue}
+          placeholder="Client city"
+          onChange={(e) => {
+            setCityValue(e.target.value);
+          }}
+          type="text"
+          required
+          pattern="[A-Za-z ]*"
+          title="Enter a valid city"
+        />
+        <label>Address</label>
+        <Input
+          name="address"
+          value={addressValue}
+          placeholder="Client address"
+          onChange={(e) => {
+            setAddressValue(e.target.value);
+          }}
+          type="address"
+          required
+          pattern="^[0-9,$], [A-Za-z ]"
+          title="Enter a valid address"
+        />
+        <label>Logo</label>
+        <Input
+          name="logo"
+          value={logoValue}
+          placeholder="Client logo"
+          onChange={(e) => {
+            setLogoValue(e.target.value);
+          }}
+          type="text"
+        />
+        <label>Description</label>
+        <Input
+          name="description"
+          value={descriptionValue}
+          placeholder="Client description"
+          onChange={(e) => {
+            setDescriptionValue(e.target.value);
+          }}
+          type="text"
+        />
+      </div>
+      <div className={styles.btn}>
+        <button type="submit" className={styles.submit}>
+          SAVE
+        </button>
+      </div>
+    </form>
   );
 };
 
