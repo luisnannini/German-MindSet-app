8f2a78f46f69d066ca0a4b6b961d6998a6c14173
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-27 22:53:29 -0300

    MS-05: Change input & labels for Input component

--- src/Components/Interviews/EditForm/index.js
+++ src/Components/Interviews/EditForm/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './editform.module.css';
+import Inputs from '../Inputs';
 
 const EditForm = (props) => {
   if (!props.show) {
@@ -8,25 +9,8 @@ const EditForm = (props) => {
   return (
     <div className={styles.form}>
       <h2>Edit</h2>
-      <h3>Postulant</h3>
-      <div className={styles.divform}>
-        <label>First Name</label>
-        <input></input>
-        <label>Last Name</label>
-        <input></input>
-      </div>
-      <h3>Client</h3>
-      <div className={styles.divform}>
-        <label>Name</label>
-        <input></input>
-        <label>Status</label>
-        <input></input>
-      </div>
-      <div className={styles.divform}>
-        <label>Date</label>
-        <input></input>
-        <label>Notes</label>
-        <input></input>
+      <div>
+        <Inputs />
       </div>
       <div className={styles.buttons}>
         <button className={styles.cancel} onClick={props.closeEditForm}>
