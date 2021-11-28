9d7bcb69d2217a286b033c7d5a8c8b16a8a1ab07
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-27 22:13:01 -0300

    MS-05: Add Edit Button/Form & Remove Button/Modal functions to show or hide the form or modal using onClick function & useState

--- src/Components/Interviews/List/index.js
+++ src/Components/Interviews/List/index.js
@@ -1,27 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './list.module.css';
 import EditButton from '../EditButton';
+import EditForm from '../EditForm';
 import RemoveButton from '../RemoveButton';
+import RemoveModal from '../RemoveModal';
 
 const List = (props) => {
-  const openModal = () => {
-    console.log('open');
+  const [showEditForm, setShowEditForm] = useState(false);
+  const [showRemoveModal, setShowRemoveModal] = useState(false);
+
+  const closeEditForm = () => {
+    setShowEditForm(false);
+  };
+
+  const confirmEditForm = () => {
+    console.log('confirm edit');
+  };
+
+  const closeRemoveModal = () => {
+    setShowRemoveModal(false);
+  };
+
+  const confirmRemoveModal = () => {
+    console.log('confirm remove');
   };
 
   return (
-    <ul className={styles.list}>
-      <li>{props.postulant.firstName}</li>
-      <li>{props.postulant.lastName}</li>
-      <li>{props.client.name}</li>
-      {/* <li>{props.application.id}</li> */}
-      <li>{props.status}</li>
-      <li>{props.date}</li>
-      <li>{props.notes}</li>
-      <li className={styles.buttons}>
-        <EditButton />
-        <RemoveButton />
-      </li>
-    </ul>
+    <div>
+      <EditForm
+        show={showEditForm}
+        closeEditForm={closeEditForm}
+        confirmEditForm={confirmEditForm}
+      />
+      <RemoveModal
+        show={showRemoveModal}
+        closeRemoveModal={closeRemoveModal}
+        confirmRemoveModal={confirmRemoveModal}
+      />
+      <ul className={styles.list}>
+        <li>{props.postulant.firstName}</li>
+        <li>{props.postulant.lastName}</li>
+        <li>{props.client.name}</li>
+        {/* <li>{props.application.id}</li> */}
+        <li>{props.status}</li>
+        <li>{props.date}</li>
+        <li>{props.notes}</li>
+      </ul>
+      <div className={styles.buttons}>
+        <EditButton onClick={() => setShowEditForm(true)} />
+        <RemoveButton onClick={() => setShowRemoveModal(true)} />
+      </div>
+    </div>
   );
 };
 
