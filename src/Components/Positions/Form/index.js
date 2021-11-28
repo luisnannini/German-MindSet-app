15c84e41c4b515436594b9b4567258eb045e24c5
nicolasblasco <nicoblasco96@gmail.com>
2021-11-27 23:14:55 -0300

    MS-06: Import Clients & Profiles to form & Modify form by passing props through other components & Modify Input component to recieve props

--- src/Components/Positions/Form/index.js
+++ src/Components/Positions/Form/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
 import Close from '../Close';
 import styles from './form.module.css';
+import CheckboxList from '../CheckboxList';
 
 const Form = ({ onClose }) => {
+  const [clients, setClients] = useState([]);
+  const [profiles, setProfiles] = useState([]);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API}/clients`)
+      .then((response) => response.json())
+      .then((response) => {
+        setClients(response.data);
+      });
+    fetch(`${process.env.REACT_APP_API}/profiles`)
+      .then((response) => response.json())
+      .then((response) => {
+        setProfiles(response.data);
+      });
+  }, []);
   return (
     <div className={styles.container}>
       <div className={styles.form}>
@@ -14,11 +31,10 @@ const Form = ({ onClose }) => {
           <Close onClick={onClose} />
         </div>
         <div className={styles.fields}>
-          <Select />
-          <Select />
-          <Select />
-          <Input />
-          <Select />
+          <Select object={clients} label={'Clients'} />
+          <Input label={'Job Description'} />
+          <Input label={'Vacancy'} />
+          <CheckboxList object={profiles} label={'Profiles'} />
         </div>
         <div className={styles.button}>
           <Button />
--- src/Components/Positions/Input/index.js
+++ src/Components/Positions/Input/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './input.module.css';
 
-const Input = () => {
+const Input = ({ label }) => {
   return (
     <div className={styles.container}>
-      <label className={styles.label}>Label</label>
+      <label className={styles.label}>{label}</label>
       <input className={styles.input}></input>
     </div>
   );
