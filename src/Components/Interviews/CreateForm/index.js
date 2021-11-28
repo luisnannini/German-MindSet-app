f812f51ac79ca5d87a831282ddc95ae9ec4315d1
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-27 21:17:16 -0300

    MS-05: Add function onClick to close form & confirm (not finished), structure form with h3, label & inputs

--- src/Components/Interviews/CreateForm/index.js
+++ src/Components/Interviews/CreateForm/index.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import styles from './createform.module.css';
 
-const CreateForm = () => {
+const CreateForm = (props) => {
+  if (!props.show) {
+    return null;
+  }
   return (
     <div className={styles.form}>
-      <p>Hello Form</p>
+      <h2>Create Interview</h2>
+      <h3>Postulant</h3>
+      <div className={styles.divform}>
+        <label>First Name</label>
+        <input></input>
+        <label>Last Name</label>
+        <input></input>
+      </div>
+      <h3>Client</h3>
+      <div className={styles.divform}>
+        <label>Name</label>
+        <input></input>
+        <label>Status</label>
+        <input></input>
+      </div>
+      <div className={styles.divform}>
+        <label>Date</label>
+        <input></input>
+        <label>Notes</label>
+        <input></input>
+      </div>
+      <div className={styles.buttons}>
+        <button className={styles.cancel} onClick={props.closeCreateForm}>
+          Cancel
+        </button>
+        <button className={styles.confirm} onClick={props.confirmCreateForm}>
+          Confirm
+        </button>
+      </div>
     </div>
   );
 };
