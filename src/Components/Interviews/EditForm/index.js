e63ca24cd5b751d8b04e98ee0bbc77e716e83e20
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-27 22:11:35 -0300

    MS-05: Add form structure to EditForm

--- src/Components/Interviews/EditForm/index.js
+++ src/Components/Interviews/EditForm/index.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import styles from './editform.module.css';
 
-const EditForm = () => {
+const EditForm = (props) => {
+  if (!props.show) {
+    return null;
+  }
   return (
     <div className={styles.form}>
-      <p>Hello Form</p>
+      <h2>Edit</h2>
+      <h3>Postulant</h3>
+      <div className={styles.divform}>
+        <label>First Name</label>
+        <input></input>
+        <label>Last Name</label>
+        <input></input>
+      </div>
+      <h3>Client</h3>
+      <div className={styles.divform}>
+        <label>Name</label>
+        <input></input>
+        <label>Status</label>
+        <input></input>
+      </div>
+      <div className={styles.divform}>
+        <label>Date</label>
+        <input></input>
+        <label>Notes</label>
+        <input></input>
+      </div>
+      <div className={styles.buttons}>
+        <button className={styles.cancel} onClick={props.closeEditForm}>
+          Cancel
+        </button>
+        <button className={styles.confirm} onClick={props.confirmEditForm}>
+          Confirm
+        </button>
+      </div>
     </div>
   );
 };
