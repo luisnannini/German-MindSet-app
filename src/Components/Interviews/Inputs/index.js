6614d7275f3d61f991ff6e70e0b7cf987539e990
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-27 23:46:40 -0300

    MS-05: Change input component to make it reusable

--- src/Components/Interviews/Inputs/index.js
+++ src/Components/Interviews/Inputs/index.js
@@ -1,31 +1,16 @@
 import React from 'react';
 import styles from './inputs.module.css';
 
-const Inputs = () => {
+const Input = (props) => {
   return (
-    <div className={styles.container}>
-      <h3>Postulant</h3>
-      <div className={styles.containerDiv}>
-        <label>First Name</label>
-        <input></input>
-        <label>Last Name</label>
-        <input></input>
-      </div>
-      <h3>Client</h3>
-      <div className={styles.containerDiv}>
-        <label>Name</label>
-        <input></input>
-        <label>Status</label>
-        <input></input>
-      </div>
-      <div className={styles.containerDiv}>
-        <label>Date</label>
-        <input></input>
-        <label>Notes</label>
-        <input></input>
-      </div>
-    </div>
+    <input
+      className={styles.input}
+      name={props.name}
+      placeholder={props.placeholder}
+      value={props.value}
+      onChange={props.onChange}
+    />
   );
 };
 
-export default Inputs;
+export default Input;
