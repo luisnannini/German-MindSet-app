7b29e85e3366db4c61482b5f09a5ef873fe172bf
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-27 22:14:44 -0300

    MS-05: Add onClick function

--- src/Components/Interviews/RemoveButton/index.js
+++ src/Components/Interviews/RemoveButton/index.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import styles from './removebutton.module.css';
 import remove from './remove.png';
-import Modal from '../Modal';
 
-const RemoveButton = () => {
-  const showModal = () => {
-    <Modal />;
-    console.log('modal');
-  };
-
+const RemoveButton = ({ onClick }) => {
   return (
     <button className={styles.button}>
-      <img
-        src={remove}
-        alt="Remove"
-        onClick={() => {
-          showModal();
-        }}
-      />
+      <img src={remove} alt="Remove" onClick={onClick} />
     </button>
   );
 };
