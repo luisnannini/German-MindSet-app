9093ce612eb6e30fcd406363c51f4f70e35ba60f
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-27 23:44:51 -0300

    MS-05: Add useState functions & inputs for firstName, lastName, name, status, date & notes

--- src/Components/Interviews/CreateForm/index.js
+++ src/Components/Interviews/CreateForm/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './createform.module.css';
+import Input from '../Inputs';
 
 const CreateForm = (props) => {
+  const [firstNameValue, setFirstNameValue] = useState('');
+  const [lastNameValue, setLastNameValue] = useState('');
+  const [nameValue, setNameValue] = useState('');
+  const [statusValue, setStatusValue] = useState('');
+  const [dateValue, setDateValue] = useState('');
+  const [notesValue, setNotesValue] = useState('');
+
   if (!props.show) {
     return null;
   }
@@ -9,28 +17,75 @@ const CreateForm = (props) => {
     <div className={styles.form}>
       <h2>Create Interview</h2>
       <h3>Postulant</h3>
-      <div className={styles.divform}>
+      <div className={styles.formDiv}>
         <label>First Name</label>
-        <input></input>
+        <Input
+          name="firstName"
+          value={firstNameValue}
+          placeholder="First Name"
+          onChange={(e) => {
+            setFirstNameValue(e.target.value);
+          }}
+          required
+        />
         <label>Last Name</label>
-        <input></input>
+        <Input
+          name="lastName"
+          value={lastNameValue}
+          placeholder="Last Name"
+          onChange={(e) => {
+            setLastNameValue(e.target.value);
+          }}
+          required
+        />
       </div>
       <h3>Client</h3>
-      <div className={styles.divform}>
+      <div className={styles.formDiv}>
         <label>Name</label>
-        <input></input>
+        <Input
+          name="name"
+          value={nameValue}
+          placeholder="Name"
+          onChange={(e) => {
+            setNameValue(e.target.value);
+          }}
+          required
+        />
         <label>Status</label>
-        <input></input>
+        <Input
+          name="status"
+          value={statusValue}
+          placeholder="Status"
+          onChange={(e) => {
+            setStatusValue(e.target.value);
+          }}
+          required
+        />
       </div>
-      <div className={styles.divform}>
+      <div className={styles.formDiv}>
         <label>Date</label>
-        <input></input>
+        <Input
+          name="date"
+          value={dateValue}
+          placeholder="Date"
+          onChange={(e) => {
+            setDateValue(e.target.value);
+          }}
+          required
+        />
         <label>Notes</label>
-        <input></input>
+        <Input
+          name="notes"
+          value={notesValue}
+          placeholder="Notes"
+          onChange={(e) => {
+            setNotesValue(e.target.value);
+          }}
+        />
       </div>
       <div className={styles.buttons}>
         <button className={styles.cancel} onClick={props.closeCreateForm}>
-          Cancel interview
+          Cancel
         </button>
         <button className={styles.confirm} onClick={props.confirmCreateForm}>
           Confirm
