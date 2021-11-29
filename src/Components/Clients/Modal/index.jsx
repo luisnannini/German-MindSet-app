a45e401445e7fb10a05a16c9f1c3eb193fd33419
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-11-29 11:19:31 -0300

    MS-04: copy the format and style of the modal example from the esteban PR

--- src/Components/Clients/Modal/index.jsx
+++ src/Components/Clients/Modal/index.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
 import styles from './modal.module.css';
-import Form from '../Form';
 
-const Modal = (props) => {
-  const { setModal, openModal } = props;
-  if (!openModal) {
+function Modal(props) {
+  if (!props.show) {
     return null;
   }
-  const handleClose = () => setModal(!openModal);
   return (
-    <div className={styles.modal}>
-      <div className={styles.buttons}>
-        <Form />
-        <button className={styles.cancel} onClick={handleClose}>
-          Cancel
-        </button>
+    <section className={styles.container}>
+      <div className={styles.modal}>
+        <h4>{props.title}</h4>
+        <div>
+          <button
+            disabled={props.isLoading}
+            onClick={props.onCancel}
+            className={`${styles.button} ${styles.cancel}`}
+          >
+            Cancel
+          </button>
+          <button disabled={props.isLoading} onClick={props.onConfirm} className={styles.button}>
+            Confirm
+          </button>
+        </div>
       </div>
-    </div>
+    </section>
   );
-};
+}
 
 export default Modal;
