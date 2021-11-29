c6fcb2e4de4a612e3dc70768cc1e0528c564b298
NicoMunoz909 <nicolas.munoz@outlook.es>
2021-11-29 05:17:51 -0300

    MS-07: Create Session component

---
+++ src/Components/Sessions/Session.js
@@ -0,0 +1,20 @@
+import { FaTrash } from 'react-icons/fa';
+import { FaPen } from 'react-icons/fa';
+import styles from './sessions.module.css';
+
+const Session = ({ postulant, psychologist, status, date, onDelete, onEdit }) => {
+  return (
+    <tr>
+      <td>{postulant}</td>
+      <td>{psychologist}</td>
+      <td>{status}</td>
+      <td>{date}</td>
+      <td>
+        <FaPen className={styles.button} onClick={() => onEdit()} />
+        <FaTrash onClick={() => onDelete()} />
+      </td>
+    </tr>
+  );
+};
+
+export default Session;
