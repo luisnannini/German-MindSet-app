234c935a48e10733b964641646da446b161aaba5
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-28 22:07:43 -0300

    MS-05: Add select postulant component

---
+++ src/Components/Interviews/SelectPostulant/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './selectpostulant.module.css';
+
+const SelectPostulant = (props) => {
+  return (
+    <div className={styles.container}>
+      <label className={styles.label}>{props.label}</label>
+      <select className={styles.select} onChange={props.onChange}>
+        {props.object.map((data) => {
+          return (
+            <option key={data._id} value={data._id}>
+              {data.firstName} {data.lastName}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default SelectPostulant;
