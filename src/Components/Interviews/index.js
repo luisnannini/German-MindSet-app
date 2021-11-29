0c6546ceac84a90643f2334cd1f529edac90d743
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-29 16:11:26 -0300

    MS-05: Add list & delete in index.js

--- src/Components/Interviews/index.js
+++ src/Components/Interviews/index.js
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import styles from './interviews.module.css';
-import List from './List';
 import CreateButton from './CreateButton';
 import CreateForm from './CreateForm';
+import EditButton from './EditButton';
+import EditForm from './EditForm';
+import RemoveButton from './RemoveButton';
+import RemoveModal from './RemoveModal';
 
 function Interviews() {
   const [interviews, setInterviews] = useState([]);
   const [showCreateForm, setShowCreateForm] = useState(false);
+  const [showEditForm, setShowEditForm] = useState(false);
+  const [showRemoveModal, setShowRemoveModal] = useState(false);
+  const [selectedInterview, setSelectedInterview] = useState(undefined);
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API}/interviews`)
@@ -16,13 +22,45 @@ function Interviews() {
       });
   }, []);
 
+  const handleDelete = (event, interview) => {
+    event.stopPropagation();
+    setSelectedInterview(interview._id);
+  };
+
   const closeCreateForm = () => {
     setShowCreateForm(false);
   };
 
+  const closeEditForm = () => {
+    setShowEditForm(false);
+  };
+
+  const closeRemoveModal = () => {
+    setShowRemoveModal(false);
+    showRemoveModal(false);
+  };
+
+  const confirmRemoveModal = () => {
+    const url = `${process.env.REACT_APP_API}/interviews/${selectedInterview}`;
+
+    fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    })
+      .then((response) => {
+        if (response.status !== 204) {
+          return response.json().then(({ message }) => {
+            throw new Error(message);
+          });
+        }
+      })
+      .catch((error) => error);
+  };
+
   return (
-    <section className={styles.container}>
-      <CreateForm show={showCreateForm} closeCreateForm={closeCreateForm} />
+    <div className={styles.container}>
       <h1>Interviews</h1>
       <ul className={styles.list1}>
         <li>Postulant</li>
@@ -38,27 +76,42 @@ function Interviews() {
         <li>Notes</li>
         <li>Actions</li>
       </ul>
-      <div>
-        {interviews.map((interview) => {
-          return (
-            <div>
-              <List
-                key={interview._id}
-                postulant={interview.postulant}
-                client={interview.client}
-                application={interview.application}
-                status={interview.status}
-                date={interview.date}
-                notes={interview.notes}
+      {interviews.map((interview) => {
+        return (
+          <ul className={styles.list} key={interview._id}>
+            <li>{interview.postulant.firstName}</li>
+            <li>{interview.postulant.lastName}</li>
+            <li>{interview.client.name}</li>
+            <li>{interview.application.result}</li>
+            <li>{interview.status}</li>
+            <li>{interview.date}</li>
+            <li>{interview.notes}</li>
+            <li className={styles.buttons}>
+              <EditButton key={interview._id} onClick={() => setShowEditForm(true)} />
+            </li>
+            <li className={styles.buttons}>
+              <RemoveButton
+                onClick={(event) => {
+                  handleDelete(event, interview);
+                  setShowRemoveModal(true);
+                }}
               />
-            </div>
-          );
-        })}
-      </div>
+            </li>
+            <EditForm show={showEditForm} closeEditForm={closeEditForm} />
+            <RemoveModal
+              show={showRemoveModal}
+              confirmRemoveModal={confirmRemoveModal}
+              closeRemoveModal={closeRemoveModal}
+            />
+          </ul>
+        );
+      })}
+
+      <CreateForm show={showCreateForm} closeCreateForm={closeCreateForm} />
       <div>
         <CreateButton onClick={() => setShowCreateForm(true)} />
       </div>
-    </section>
+    </div>
   );
 }
 
