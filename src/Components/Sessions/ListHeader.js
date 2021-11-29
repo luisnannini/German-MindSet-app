8048c6912c1fd3962425080ccba72b31da8dc3eb
NicoMunoz909 <nicolas.munoz@outlook.es>
2021-11-29 05:19:05 -0300

    MS-07: Create list header component

---
+++ src/Components/Sessions/ListHeader.js
@@ -0,0 +1,16 @@
+const ListHeader = () => {
+  return (
+    <thead>
+      <tr>
+        <th>Postulant</th>
+        <th>Psychologist</th>
+        <th>Status</th>
+        <th>Date</th>
+        <th>Actions</th>
+        <th></th>
+      </tr>
+    </thead>
+  );
+};
+
+export default ListHeader;
