ad5fee801d9371b11d4507595d66b7427a54ef5b
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-29 18:00:06 -0300

    MS-05: Add default value to options

--- src/Components/Interviews/SelectApplication/index.js
+++ src/Components/Interviews/SelectApplication/index.js
@@ -5,6 +5,9 @@ const SelectApplication = (props) => {
   return (
     <div className={styles.container}>
       <select className={styles.select} onChange={props.onChange}>
+        <option value="0" selected="selected">
+          Id
+        </option>
         {props.object.map((data) => {
           return (
             <option key={data._id} value={data._id}>
