69c6f4947d7dfde2e7b539d3837e45d5565307f7
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-28 22:07:13 -0300

    MS-05: Add select client component

---
+++ src/Components/Interviews/SelectClient/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './selectclient.module.css';
+
+const SelectClient = (props) => {
+  return (
+    <div className={styles.container}>
+      <label className={styles.label}>{props.label}</label>
+      <select className={styles.select} onChange={props.onChange}>
+        {props.object.map((data) => {
+          return (
+            <option key={data._id} value={data._id}>
+              {data.name}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default SelectClient;
