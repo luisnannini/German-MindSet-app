5a108de691ef44510af625bbb71206136661b601
NicoMunoz909 <nicolas.munoz@outlook.es>
2021-11-29 05:20:27 -0300

    MS-07: Create header component

---
+++ src/Components/Sessions/Header.js
@@ -0,0 +1,13 @@
+/* eslint-disable semi */
+/* eslint-disable prettier/prettier */
+import React from 'react'
+
+const Header = () => {
+  return (
+    <h2>
+      Sessions
+    </h2>
+  )
+}
+
+export default Header
