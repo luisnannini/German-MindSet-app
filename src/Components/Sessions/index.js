58fc8e62891bde1ee0f23c568235950962554e5a
NicoMunoz909 <nicolas.munoz@outlook.es>
2021-11-29 05:23:53 -0300

    MS-07: Implement request to get all sessions and show them on a table

--- src/Components/Sessions/index.js
+++ src/Components/Sessions/index.js
@@ -1,10 +1,51 @@
+import { useEffect, useState } from 'react';
 import styles from './sessions.module.css';
+import Header from './Header';
+import Session from './Session';
+import ListHeader from './ListHeader';
 
 function Sessions() {
+  const [Sessions, setSessions] = useState([]);
+
+  useEffect(() => {
+    getSessions();
+  }, []);
+
+  function getSessions() {
+    fetch(`${process.env.REACT_APP_API}/sessions`)
+      .then((response) => response.json())
+      .then((response) => {
+        setSessions(response.data);
+      });
+  }
+
   return (
-    <section className={styles.container}>
-      <h2>Sessions</h2>
-    </section>
+    <div className={styles.container}>
+      <section>
+        <Header />
+        <table>
+          <ListHeader />
+          <tbody>
+            {Sessions.map((session) => {
+              return (
+                <Session
+                  key={session._id}
+                  id={session._id}
+                  postulant={`${session.postulant.firstName} ${session.postulant.lastName}`}
+                  psychologist={
+                    session.psychologist
+                      ? `${session.psychologist.firstName} ${session.psychologist.lastName}`
+                      : 'Unassigned'
+                  }
+                  status={session.status}
+                  date={session.date}
+                />
+              );
+            })}
+          </tbody>
+        </table>
+      </section>
+    </div>
   );
 }
 
