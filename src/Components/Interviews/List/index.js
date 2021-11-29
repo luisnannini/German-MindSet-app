6cb024f27e9361a4520bacc0d8b5564d2946cbce
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-29 13:35:46 -0300

    MS-05: Add delete method but still not working

--- src/Components/Interviews/List/index.js
+++ src/Components/Interviews/List/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './list.module.css';
 import EditButton from '../EditButton';
 import EditForm from '../EditForm';
@@ -6,32 +6,48 @@ import RemoveButton from '../RemoveButton';
 import RemoveModal from '../RemoveModal';
 
 const List = (props) => {
+  const [interviews, setInterviews] = useState([]);
   const [showEditForm, setShowEditForm] = useState(false);
   const [showRemoveModal, setShowRemoveModal] = useState(false);
 
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API}/interviews`)
+      .then((response) => response.json())
+      .then((response) => {
+        setInterviews(response.data);
+      });
+  }, []);
+
   const closeEditForm = () => {
     setShowEditForm(false);
-  };
-
-  const confirmEditForm = () => {
-    console.log('confirm edit');
   };
 
   const closeRemoveModal = () => {
     setShowRemoveModal(false);
   };
 
-  const confirmRemoveModal = () => {
-    console.log('confirm remove');
+  const confirmRemoveModal = (id) => {
+    const url = `${process.env.REACT_APP_API}/interviews/${id}`;
+
+    fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    })
+      .then((response) => {
+        if (response.status !== 204) {
+          return response.json().then(({ message }) => {
+            throw new Error(message);
+          });
+        }
+      })
+      .catch((error) => error);
   };
 
   return (
     <div>
-      <EditForm
-        show={showEditForm}
-        closeEditForm={closeEditForm}
-        confirmEditForm={confirmEditForm}
-      />
+      <EditForm show={showEditForm} closeEditForm={closeEditForm} />
       <RemoveModal
         show={showRemoveModal}
         closeRemoveModal={closeRemoveModal}
@@ -41,7 +57,7 @@ const List = (props) => {
         <li>{props.postulant.firstName}</li>
         <li>{props.postulant.lastName}</li>
         <li>{props.client.name}</li>
-        <li>{props.application._id}</li>
+        <li>{props.application.result}</li>
         <li>{props.status}</li>
         <li>{props.date}</li>
         <li>{props.notes}</li>
