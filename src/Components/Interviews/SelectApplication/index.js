6833dcdd37b2475f0d74490a6f3e76a326da24a6
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-29 00:43:41 -0300

    MS-05: create select application component

---
+++ src/Components/Interviews/SelectApplication/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './selectapplication.module.css';
+
+const SelectApplication = (props) => {
+  return (
+    <div className={styles.container}>
+      <select className={styles.select} onChange={props.onChange}>
+        {props.object.map((data) => {
+          return (
+            <option key={data._id} value={data._id}>
+              {data._id}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default SelectApplication;
