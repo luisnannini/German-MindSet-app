8399f1721a8ca7b8ee65bb15a418d064ee302e7b
ValentinaCollareta <valencollareta2@gmail.com>
2021-11-29 21:15:38 -0300

    MS-05: fix select value

--- src/Components/Interviews/SelectClient/index.js
+++ src/Components/Interviews/SelectClient/index.js
@@ -4,10 +4,8 @@ import styles from './selectclient.module.css';
 const SelectClient = (props) => {
   return (
     <div className={styles.container}>
-      <select className={styles.select} onChange={props.onChange}>
-        <option value="0" selected="selected">
-          Client Name
-        </option>
+      <select className={styles.select} value={props.value} onChange={props.onChange}>
+        <option value="0">Client Name</option>
         {props.object.map((data) => {
           return (
             <option key={data._id} value={data._id}>
