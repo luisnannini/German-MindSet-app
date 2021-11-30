20c858a292bd628762253abe5a691e30c581e5ad
Pablo Balbo <pablobalbo6@gmail.com>
2021-11-30 01:36:36 -0300

    MS-02: Add crud features

--- src/Components/Admins/Modal/index.js
+++ src/Components/Admins/Modal/index.js
@@ -1,25 +1,26 @@
-import React from 'react';
 import styles from './modal.module.css';
-import Button from '../Button';
 
 const Modal = (props) => {
+  const { onCancel, onConfirm, isLoading, show } = props;
+  if (!show) {
+    return null;
+  }
   return (
-    <React.Fragment>
-      <div className={props.visible ? '' : styles.hideModal}>
-        <div className={styles.container}>
-          <div className={styles.form}>
-            <div className={styles.header}>
-              <h2>Are you sure you want to delete this administrator?</h2>
-              <div>{props.admin}</div>
-            </div>
-            <div className={styles.header}>
-              <Button class={styles.greenButton} action={props.action} name={'Confirm'} />
-              <Button class={styles.redButton} action={props.cancel} name={'Cancel'} />
-            </div>
-          </div>
+    <div className={styles.container}>
+      <div className={styles.form}>
+        <div className={styles.header}>
+          <h2>Are you sure you want to delete this administrator?</h2>
+        </div>
+        <div className={styles.header}>
+          <button disabled={isLoading} onClick={() => onCancel()}>
+            Cancel
+          </button>
+          <button disabled={isLoading} onClick={() => onConfirm()}>
+            Confirm
+          </button>
         </div>
       </div>
-    </React.Fragment>
+    </div>
   );
 };
 
