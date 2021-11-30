6143fff8e804263ecfdea3342e1e2f72f9860f57
nicolasblasco <nicoblasco96@gmail.com>
2021-11-30 00:08:33 -0300

    MS-08: Create list with retrieved profiles. Add function & code to delete a profile. Add code to open and close delete modal. Add style to list.

--- src/Components/Profiles/index.js
+++ src/Components/Profiles/index.js
@@ -1,9 +1,99 @@
+import { useState, useEffect } from 'react';
 import styles from './profiles.module.css';
+import Modal from './Modal';
+import CreateButton from './CreateButton';
+import UpdateButton from './UpdateButton';
+import DeleteButton from './DeleteButton';
 
 function Profiles() {
+  const [profiles, setProfiles] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedProfile, setSelectedProfile] = useState(undefined);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API}/profiles`)
+      .then((response) => response.json())
+      .then((response) => {
+        setProfiles(response.data);
+      });
+  }, []);
+
+  const handleDelete = (event, profile) => {
+    event.stopPropagation();
+    setSelectedProfile(profile._id);
+    setShowModal(true);
+  };
+
+  const deleteProfile = () => {
+    fetch(`${process.env.REACT_APP_API}/profiles/${selectedProfile}`, { method: 'DELETE' })
+      .then((response) => {
+        if (response.status !== 204) {
+          return response.json().then(({ message }) => {
+            throw new Error(message);
+          });
+        }
+        return fetch(`${process.env.REACT_APP_API}/profiles`)
+          .then((response) => {
+            if (response.status !== 200) {
+              return response.json().then(({ message }) => {
+                throw new Error(message);
+              });
+            }
+            return response.json();
+          })
+          .then((response) => {
+            setProfiles(response.data);
+            closeModal();
+          });
+      })
+      .catch((error) => error);
+  };
+
+  const showForm = (profile) => {
+    if (profile) {
+      window.location.href = `profiles/form?id=${profile._id}`;
+    } else {
+      window.location.href = `profiles/form`;
+    }
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   return (
-    <section className={styles.container}>
-      <h2>Profiles</h2>
+    <section>
+      <Modal
+        show={showModal}
+        title="Delete a Profile"
+        message="Are you sure you want to delete this session?"
+        onCancel={closeModal}
+        onConfirm={deleteProfile}
+      />
+      <div className={styles.container}>
+        <h2 className={styles.title}>Profiles</h2>
+        <ul className={styles.listHeader}>
+          <li>Profiles</li>
+          <li></li>
+          <li></li>
+        </ul>
+        {profiles.map((profile) => {
+          return (
+            <ul key={profile._id} className={styles.list}>
+              <li>{profile.name}</li>
+              <li>
+                <UpdateButton onClick={() => showForm(profile)} />
+              </li>
+              <li>
+                <DeleteButton onClick={(event) => handleDelete(event, profile)} />
+              </li>
+            </ul>
+          );
+        })}
+      </div>
+      <div className={styles.button}>
+        <CreateButton onClick={() => showForm()} />
+      </div>
     </section>
   );
 }
