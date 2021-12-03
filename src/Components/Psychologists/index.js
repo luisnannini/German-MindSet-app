83044f0f887496294a60d0b242a2e4d3c6f688fe
Pablo Bernal <semperfi1992.pb@gmail.com>
2021-12-03 02:45:19 -0300

    MS-09: Added titles for the list columns

--- src/Components/Psychologists/index.js
+++ src/Components/Psychologists/index.js
@@ -28,6 +28,18 @@ function Psychologists() {
         <h2>{field}</h2>
       </div>
       <div>
+        <ul className={styles.listElem}>
+          <li>First Name</li>
+          <li>Last Name</li>
+          <li>Username</li>
+          <li>Email</li>
+          <li>Phone Number</li>
+          <li>Address</li>
+          <li>EDIT</li>
+          <li>DELETE</li>
+        </ul>
+      </div>
+      <div>
         {psychologists.map((psy) => {
           return <PsyList name={psy} key={psy._id} />;
         })}
