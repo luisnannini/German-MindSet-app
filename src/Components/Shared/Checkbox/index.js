afaa7d35e03bfffb2170b3d376051e11a1753fdb
Pablo Balbo <pablobalbo6@gmail.com>
2021-12-06 02:32:15 -0300

    MS-17: Checkbox fixed

--- src/Components/Shared/Checkbox/index.js
+++ src/Components/Shared/Checkbox/index.js
@@ -4,13 +4,14 @@ import styles from './checkbox.module.css';
 const Checkbox = (props) => {
   return (
     <div className={styles.container}>
-        <label className={styles.label}>{props.label}</label>
-        <Checkbox>
-            className={props.checkbox}
-            onChange={props.onChange}
-            disabled={props.isDiseabled}
-            checked={props.checked}
-        </Checkbox>
+      <label className={styles.label}>{props.label}</label>
+      <input
+        type={'checkbox'}
+        className={styles.checkbox}
+        onChange={props.onChange}
+        checked={props.value}
+        disabled={props.checked}
+      ></input>
     </div>
   );
 };
