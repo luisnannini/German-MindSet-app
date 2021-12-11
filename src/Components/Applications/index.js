64473a6693ae04f21cfbfbc0ceb55110b61c03ae
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2021-12-11 20:45:55 -0300

    MS-33: index file of applications resource updated acording to redux

--- src/Components/Applications/index.js
+++ src/Components/Applications/index.js
@@ -1,95 +1,58 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import styles from './applications.module.css';
 import ButtonCreate from '../Shared/Buttons/ButtonCreate';
 import ButtonDelete from '../Shared/Buttons/ButtonDelete';
 import Modal from '../Shared/Modal';
 import ModalError from '../Shared/ModalError';
+import { useDispatch, useSelector } from 'react-redux';
+import { getApplications, deleteApplication } from '../../redux/Applications/thunks';
+import { applicationsErrorModal } from '../../redux/Applications/actions';
 
 function Applications() {
-  const [applications, setApplications] = useState([]);
+  const [selectedApplication, setSelectedApplication] = useState(undefined);
   const [showDelete, setShowDelete] = useState(false);
-  const [isLoading, setLoading] = useState(false);
-  const [error, setError] = useState({
-    show: false,
-    message: '',
-    title: ''
-  });
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const applications = useSelector((store) => store.applications.list);
+  const isLoading = useSelector((store) => store.applications.isLoading);
+  const error = useSelector((store) => store.applications.error);
 
   useEffect(() => {
-    setLoading(true);
-    fetch(`${process.env.REACT_APP_API}/applications`)
-      .then((response) => {
-        if (response.status !== 200 && response.status !== 201 && response.status !== 204) {
-          const status = `${response.status} ${response.statusText}`;
-          return response.json().then(({ message }) => {
-            if (message.message) throw { message: message.message, status };
-            throw { message, status };
-          });
-        }
-        return response.json();
-      })
-      .then((response) => {
-        setApplications(response.data);
-      })
-      .catch((error) => setError({ show: true, message: error.message, title: error.status }))
-      .finally(() => setLoading(false));
-  }, []);
+    if (!applications.length) {
+      dispatch(getApplications());
+    }
+  }, [applications]);
 
-  const deleteApplications = async (id) => {
-    setShowDelete(false);
-    setLoading(true);
-    await fetch(`${process.env.REACT_APP_API}/applications/${id}`, {
-      method: 'DELETE'
-    })
-      .then((response) => {
-        if (response.status !== 200 && response.status !== 201 && response.status !== 204) {
-          const status = `${response.status} ${response.statusText}`;
-          return response.json().then(({ message }) => {
-            if (message.message) throw { message: message.message, status };
-            throw { message, status };
-          });
-        }
-        return fetch(`${process.env.REACT_APP_API}/applications`)
-          .then((response) => {
-            if (response.status !== 200 && response.status !== 201 && response.status !== 204) {
-              const status = `${response.status} ${response.statusText}`;
-              return response.json().then(({ message }) => {
-                if (message.message) throw { message: message.message, status };
-                throw { message, status };
-              });
-            }
-            return response.json();
-          })
-          .then((response) => {
-            setApplications(response.data);
-          });
-      })
-      .catch((error) => setError({ show: true, message: error.message, title: error.status }))
-      .finally(() => setLoading(false));
+  const handleDelete = (event, application) => {
+    event.stopPropagation();
+    setSelectedApplication(application._id);
+    setShowDelete(true);
+  };
+
+  const handleConfirm = () => {
+    dispatch(deleteApplication(selectedApplication)).then(() => {
+      setSelectedApplication(undefined);
+      setShowDelete(false);
+    });
   };
 
   return (
     <section className={styles.section}>
       <Modal
-        show={!!showDelete}
+        show={showDelete}
         title="Delete Application"
         message="Are you sure you want to delete this application?"
-        onCancel={() => {
-          setShowDelete(false);
-        }}
-        onConfirm={() => {
-          deleteApplications(showDelete);
-          setShowDelete(false);
-        }}
+        onConfirm={() => handleConfirm}
+        onCancel={() => setShowDelete(false)}
       />
-      <ModalError error={error} onConfirm={() => setError({ show: false })} />
+      <ModalError error={error} onConfirm={() => dispatch(applicationsErrorModal())} />
       <div className={styles.container}>
         <div className={styles.header}>
           <h2 className={styles.title}>Applications</h2>
-          <Link to="/applications/form">
-            <ButtonCreate disabled={isLoading} />
-          </Link>
+          <ButtonCreate disabled={isLoading} onClick={() => history.push('/applications/form')} />
         </div>
         <table className={styles.table}>
           <thead>
@@ -113,7 +76,10 @@ function Applications() {
                 <td>{app.interview._id}</td>
                 <td>{app.result}</td>
                 <td>
-                  <ButtonDelete onClick={() => setShowDelete(app._id)} disabled={isLoading} />
+                  <ButtonDelete
+                    onClick={(event) => handleDelete(event, app)}
+                    disabled={isLoading}
+                  />
                 </td>
               </tr>
             ))}
