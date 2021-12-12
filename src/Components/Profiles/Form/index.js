43c305d6290911710682d119756f3315cca48e64
nicolasblasco <nicoblasco96@gmail.com>
2021-12-12 18:43:18 -0300

    MS-39: Add useHistory to cancel button in form

--- src/Components/Profiles/Form/index.js
+++ src/Components/Profiles/Form/index.js
@@ -1,5 +1,4 @@
 import { React, useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
 import useQuery from '../../../Hooks/useQuery';
 import styles from './form.module.css';
@@ -81,9 +80,7 @@ const Form = () => {
           </div>
         </div>
         <div className={styles.button}>
-          <Link to="/profiles">
-            <ButtonCancel disabled={isLoading} />
-          </Link>
+          <ButtonCancel disabled={isLoading} onClick={() => history.push('/profiles')} />
           <ButtonConfirm disabled={isLoading} type={'submit'} />
         </div>
         <ModalError error={error} onConfirm={() => dispatch(closeErrorModal())} />
