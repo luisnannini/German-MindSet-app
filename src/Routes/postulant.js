3caa22cbd0093f9507c44bb3e64192b82157450d
ValentinaCollareta <valencollareta2@gmail.com>
2021-12-17 14:40:36 -0300

    MS-54: Edit Layout, add Postulant Profile & Home

--- src/Routes/postulant.js
+++ src/Routes/postulant.js
@@ -1,13 +1,21 @@
 import { Switch, Route, useRouteMatch } from 'react-router-dom';
 import Home from '../Components/Postulants/Home';
+import Profile from '../Components/Postulants/Profile';
 import Layout from '../Components/Layout';
 
 const AdminRoutes = () => {
   const { url } = useRouteMatch();
   return (
-    <Layout routes={[{ name: 'Go to admin app', path: '/admin' }]}>
+    <Layout
+      routes={[
+        { name: 'Home', path: '/postulant/' },
+        { name: 'Go to admin app', path: '/admin' },
+        { name: 'Postulant Profile', path: '/postulant/profile' }
+      ]}
+    >
       <Switch>
-        <Route path={`${url}/`} component={Home} />
+        <Route exact path={`${url}/`} component={Home} />
+        <Route path={`${url}/profile`} component={Profile} />
       </Switch>
     </Layout>
   );
