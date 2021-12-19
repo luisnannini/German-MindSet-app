2ef71f955d9f006f78a8e70dffbec47f2bdd2668
luisnannini <luisnannini97@gmail.com>
2021-12-19 16:42:31 -0300

    MS-51: add final form logic

--- src/Components/Admin/Clients/Form/index.js
+++ src/Components/Admin/Clients/Form/index.js
@@ -9,8 +9,9 @@ import Input from '../../../Shared/Input';
 import ButtonCancel from '../../../Shared/Buttons/ButtonCancel';
 import ButtonConfirm from '../../../Shared/Buttons/ButtonConfirm';
 import ModalError from '../../../Shared/Modals/ModalError';
+import { Form, Field } from 'react-final-form';
 
-function Form() {
+function ClientsForm() {
   const history = useHistory();
   const dispatch = useDispatch();
   const [clientId, setClientId] = useState(undefined);
@@ -43,40 +44,39 @@ function Form() {
     }
   }, []);
 
-  const onChangeNameInput = (event) => {
-    setNameValue(event.target.value);
-  };
+  // const onChangeNameInput = (event) => {
+  //   setNameValue(event.target.value);
+  // };
 
-  const onChangePhoneInput = (event) => {
-    setPhoneValue(event.target.value);
-  };
+  // const onChangePhoneInput = (event) => {
+  //   setPhoneValue(event.target.value);
+  // };
 
-  const onChangeCountryInput = (event) => {
-    setCountryValue(event.target.value);
-  };
+  // const onChangeCountryInput = (event) => {
+  //   setCountryValue(event.target.value);
+  // };
 
-  const onChangeStateInput = (event) => {
-    setStateValue(event.target.value);
-  };
+  // const onChangeStateInput = (event) => {
+  //   setStateValue(event.target.value);
+  // };
 
-  const onChangeCityInput = (event) => {
-    setCityValue(event.target.value);
-  };
+  // const onChangeCityInput = (event) => {
+  //   setCityValue(event.target.value);
+  // };
 
-  const onChangeAddressInput = (event) => {
-    setAddressValue(event.target.value);
-  };
+  // const onChangeAddressInput = (event) => {
+  //   setAddressValue(event.target.value);
+  // };
 
-  const onChangeLogoInput = (event) => {
-    setLogoValue(event.target.value);
-  };
+  // const onChangeLogoInput = (event) => {
+  //   setLogoValue(event.target.value);
+  // };
 
-  const onChangeDescriptionInput = (event) => {
-    setDescriptionValue(event.target.value);
-  };
+  // const onChangeDescriptionInput = (event) => {
+  //   setDescriptionValue(event.target.value);
+  // };
 
-  const submitClients = (event) => {
-    event.preventDefault();
+  const submitClients = (formValues) => {
     const clientId = query.get('_id');
     if (clientId) {
       dispatch(
@@ -98,126 +98,165 @@ function Form() {
     } else {
       dispatch(
         createClient({
-          name: nameValue,
-          phone: parseInt(phoneValue),
+          name: formValues.name,
+          phone: parseInt(formValues.phone),
           location: {
-            country: countryValue,
-            state: stateValue,
-            city: cityValue,
-            address: addressValue
+            country: formValues.country,
+            state: formValues.state,
+            city: formValues.city,
+            address: formValues.address
           },
-          logo: logoValue,
-          description: descriptionValue
+          logo: formValues.logo,
+          description: formValues.description
         })
       ).then((response) => {
         if (response) history.push('/admin/clients');
       });
     }
   };
+  const validate = (formValues) => {
+    const errors = {};
+    // Name
+    if (!formValues.name?.match(/^[a-zA-Z]+$/)) {
+      errors.name = 'First name must contain only letters';
+    }
+    if (formValues.name?.length < 3) {
+      errors.name = 'First name must be at least 3 letters';
+    }
+    // Address
+    if (formValues.address?.search(/[a-zA-Z]/) < 0 || formValues.address?.search(/[0-9]/) < 0) {
+      errors.address = 'Address must contain a name and a number';
+    }
+    // Phone Number
+    if (!formValues.phone?.toString().match(/^\d+$/)) {
+      errors.phone = 'Phone number must contain only numbers';
+    }
+    if (formValues.phone?.length < 7 || formValues.phone?.length > 14) {
+      errors.phone = 'Phone number must be between 7 and 14 numbers';
+    }
+    // Country
+    if (formValues.country?.length < 5) {
+      errors.country = 'School must contain at least 5 characters';
+    }
+    if (formValues.country?.length > 50) {
+      errors.country = 'School must be less than 50 characters';
+    }
+    // City
+    if (formValues.city?.length < 3) {
+      errors.city = 'City must contain at least 3 characters';
+    }
+    if (formValues.city?.length > 50) {
+      errors.city = 'City must be less than 50 characters';
+    }
+    // State
+    if (formValues.state?.length < 3) {
+      errors.state = 'State must contain at least 3 characters';
+    }
+    if (formValues.state?.length > 50) {
+      errors.state = 'State must be less than 50 characters';
+    }
+    return errors;
+  };
+
+  const required = (value) => (value ? undefined : 'Required');
 
   return (
     <div className={styles.container}>
-      <form className={styles.form} onSubmit={submitClients}>
-        <div className={styles.header}>
-          <h2 className={styles.title}>{clientId ? 'Update Client' : 'Create a Client'}</h2>
-        </div>
-        <div className={styles.fields}>
-          <div className={styles.columns}>
-            <Input
-              label={'Name'}
-              name={'name'}
-              type={'text'}
-              value={nameValue}
-              placeholder={'Insert Client name...'}
-              pattern={'[A-Za-z ]*'}
-              onChange={onChangeNameInput}
-              required={true}
-              disabled={isLoading}
-            />
-            <Input
-              label={'Address'}
-              name={'address'}
-              type={'address'}
-              value={addressValue}
-              placeholder={'Insert Client address...'}
-              onChange={onChangeAddressInput}
-              required={true}
-              disabled={isLoading}
-            />
-            <Input
-              label={'Country'}
-              name={'country'}
-              type={'text'}
-              value={countryValue}
-              placeholder={'Insert Client country...'}
-              pattern={'[A-Za-z ]*'}
-              onChange={onChangeCountryInput}
-              required={true}
-              disabled={isLoading}
-            />
-            <Input
-              label={'Logo'}
-              name={'logo'}
-              type={'text'}
-              value={logoValue}
-              placeholder={'Insert Client logo...'}
-              onChange={onChangeLogoInput}
-              disabled={isLoading}
-            />
-          </div>
-          <div className={styles.columns}>
-            <Input
-              label={'Phone Number'}
-              name={'phone'}
-              type={'number'}
-              value={phoneValue}
-              placeholder={'Insert Client phone...'}
-              pattern={'[0-9]'}
-              onChange={onChangePhoneInput}
-              required={true}
-              disabled={isLoading}
-            />
-            <Input
-              label={'City'}
-              name={'city'}
-              type={'text'}
-              value={cityValue}
-              placeholder={'Insert Client city...'}
-              pattern={'[A-Za-z ]*'}
-              onChange={onChangeCityInput}
-              required={true}
-              disabled={isLoading}
-            />
-            <Input
-              label={'State'}
-              name={'state'}
-              type={'text'}
-              value={stateValue}
-              placeholder={'Insert Client state...'}
-              pattern={'[A-Za-z ]*'}
-              onChange={onChangeStateInput}
-              required={true}
-              disabled={isLoading}
-            />
-            <Input
-              label={'Description'}
-              name={'description'}
-              type={'text'}
-              value={descriptionValue}
-              placeholder={'Insert Client description...'}
-              onChange={onChangeDescriptionInput}
-              disabled={isLoading}
-            />
-          </div>
-        </div>
-        <div className={styles.button}>
-          <ButtonCancel disabled={isLoading} onClick={() => history.push('/admin/clients')} />
-          <ButtonConfirm disabled={isLoading} type="submit" />
-        </div>
-        <ModalError error={error} onConfirm={() => dispatch(closeErrorModal())} />
-      </form>
+      <ModalError error={error} onConfirm={() => dispatch(closeErrorModal())} />
+      <Form onSubmit={submitClients} validate={validate}>
+        {(formProps) => (
+          <form className={styles.form} onSubmit={formProps.handleSubmit}>
+            <div className={styles.header}>
+              <h2 className={styles.title}>{clientId ? 'Update Client' : 'Create a Client'}</h2>
+            </div>
+            <div className={styles.fields}>
+              <div className={styles.columns}>
+                <Field
+                  label={'Name'}
+                  name={'name'}
+                  initialValue={nameValue}
+                  placeholder={'Insert Client name...'}
+                  disabled={isLoading}
+                  component={Input}
+                  validate={required}
+                />
+                <Field
+                  label={'Address'}
+                  name={'address'}
+                  type={'address'}
+                  initialValue={addressValue}
+                  placeholder={'Insert Client address...'}
+                  disabled={isLoading}
+                  component={Input}
+                  validate={required}
+                />
+                <Field
+                  label={'Country'}
+                  name={'country'}
+                  initialValue={countryValue}
+                  placeholder={'Insert Client country...'}
+                  disabled={isLoading}
+                  component={Input}
+                  validate={required}
+                />
+                <Field
+                  label={'Logo'}
+                  name={'logo'}
+                  initialValue={logoValue}
+                  placeholder={'Insert Client logo...'}
+                  disabled={isLoading}
+                  component={Input}
+                />
+              </div>
+              <div className={styles.columns}>
+                <Field
+                  label={'Phone Number'}
+                  name={'phone'}
+                  type={'number'}
+                  initialValue={phoneValue}
+                  placeholder={'Insert Client phone...'}
+                  disabled={isLoading}
+                  component={Input}
+                  validate={required}
+                />
+                <Field
+                  label={'City'}
+                  name={'city'}
+                  initialValue={cityValue}
+                  placeholder={'Insert Client city...'}
+                  disabled={isLoading}
+                  component={Input}
+                  validate={required}
+                />
+                <Field
+                  label={'State'}
+                  name={'state'}
+                  initialValue={stateValue}
+                  placeholder={'Insert Client state...'}
+                  disabled={isLoading}
+                  component={Input}
+                  validate={required}
+                />
+                <Field
+                  label={'Description'}
+                  name={'description'}
+                  initialValue={descriptionValue}
+                  placeholder={'Insert Client description...'}
+                  disabled={isLoading}
+                  component={Input}
+                />
+              </div>
+            </div>
+            <div className={styles.button}>
+              <ButtonCancel disabled={isLoading} onClick={() => history.push('/admin/clients')} />
+              <ButtonConfirm disabled={isLoading} type="submit" />
+            </div>
+            <ModalError error={error} onConfirm={() => dispatch(closeErrorModal())} />
+          </form>
+        )}
+      </Form>
     </div>
   );
 }
 
-export default Form;
+export default ClientsForm;
