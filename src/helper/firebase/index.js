f9b049a8e92ea4d91111bcf0549cb4f4f99ab1bf
nicolasblasco <nicoblasco96@gmail.com>
2022-01-04 20:33:11 -0300

    MS-62: Fix route to import setAuthentication

--- src/helper/firebase/index.js
+++ src/helper/firebase/index.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import { store } from 'redux/store';
-import { setAuthentication } from 'redux/auth/actions';
+import { setAuthentication } from 'redux/Auth/actions';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
