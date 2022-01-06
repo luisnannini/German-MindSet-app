3e91a8d7060941bee7c222fcef4eca225ae0c27f
ValentinaCollareta <valencollareta2@gmail.com>
2022-01-06 17:54:51 -0300

    MS-53: Add homeroutes, upgrade others routes to add home, upgrade home & postulantsHome components, add open jobs to both components, add Logout to postulants home (not working yet)

--- src/Components/Postulants/Home/index.js
+++ src/Components/Postulants/Home/index.js
@@ -1,45 +1,34 @@
 import styles from './home.module.css';
-import img from 'Images/img-home.jpg';
-// import { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { getPositions } from 'redux/Positions/thunks';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPositions } from 'redux/Positions/thunks';
 
 function Home() {
-  // const dispatch = useDispatch();
-  // const positions = useSelector((store) => store.positions.list);
-  // useEffect(() => {
-  //   if (!positions.length) {
-  //     dispatch(getPositions());
-  //   }
-  // }, [positions]);
+  const dispatch = useDispatch();
+  const positions = useSelector((store) => store.positions.list);
+  useEffect(() => {
+    if (!positions.length) {
+      dispatch(getPositions());
+    }
+  }, [positions]);
   return (
     <section className={styles.container}>
-      <img src={img} alt="Img Home"></img>
       <h2>Open Jobs</h2>
-      <table className={styles.table}>
-        <thead>
-          <tr>
-            <th>Client</th>
-            <th>Profiles</th>
-            <th>Job Description</th>
-            <th>Vacancy</th>
-            <th>Is Open</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* {positions.map((position) => {
+      <div className={styles.openJobs}>
+        <p>{positions.isOpen === false && 'No open jobs'}</p>
+        {positions.map((position) => {
+          if (position.isOpen === true) {
             return (
-              <tr key={position._id}>
-                <td>{position.client.name}</td>
-                <td>{position.professionalProfiles.name}</td>
-                <td>{position.jobDescription}</td>
-                <td>{position.vacancy}</td>
-                <td>{position.isOpen ? 'Yes' : 'No'}</td>
-              </tr>
+              <div className={styles.openJobsDiv}>
+                <p className={styles.clientName}>{position.client.name}</p>
+                <span>{position.professionalProfiles.name}</span>
+                <span>{position.jobDescription}</span>
+                <span>Vacancies: {position.vacancy}</span>
+              </div>
             );
-          })} */}
-        </tbody>
-      </table>
+          }
+        })}
+      </div>
     </section>
   );
 }
