1805655b63d394c7712378250048c91cdfa34668
luisnannini <luisnannini97@gmail.com>
2022-01-12 17:12:19 -0300

    MS-68: add Interviews component to postulant, with its route

--- src/Routes/postulant.js
+++ src/Routes/postulant.js
@@ -2,6 +2,7 @@ import { Switch, Route, useRouteMatch, Redirect } from 'react-router-dom';
 import Home from 'Components/Postulants/Home';
 import Profile from 'Components/Postulants/Profile';
 import EditProfile from 'Components/Postulants/Profile/Form';
+import Interviews from 'Components/Postulants/Interviews';
 import Layout from 'Components/Layout';
 
 const PostulantRoutes = () => {
@@ -11,6 +12,7 @@ const PostulantRoutes = () => {
       routes={[
         { name: 'Home', path: '/postulant/home' },
         { name: 'Postulant Profile', path: '/postulant/profile' },
+        { name: 'Interviews', path: '/postulant/interviews' },
         { name: 'Logout', path: '/logout' }
       ]}
     >
@@ -18,6 +20,7 @@ const PostulantRoutes = () => {
         <Route exact path={`${url}/home`} component={Home} />
         <Route exact path={`${url}/profile`} component={Profile} />
         <Route path={`${url}/profile/form`} component={EditProfile} />
+        <Route path={`${url}/interviews`} component={Interviews} />
         {/* <Route path={`${url}/logout`} component={Logout} /> */}
         <Redirect to={`${url}/home`} />
       </Switch>
