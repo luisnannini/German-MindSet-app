710fb876e185f9f8c6d4f5b09746194a4cc51b35
ValentinaCollareta <valencollareta2@gmail.com>
2022-01-16 18:50:10 -0300

    MS-67: Create session component & add to postulant routes

---
+++ src/Components/Postulants/Sessions/index.js
@@ -0,0 +1,52 @@
+import styles from './sessions.module.css';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getSessions } from 'redux/Sessions/thunks';
+
+function Session() {
+  const dispatch = useDispatch();
+  const sessions = useSelector((store) => store.sessions.list);
+  const postulantId = useSelector((store) => store.auth.data._id);
+  const postulant = useSelector((store) => store.auth.data);
+
+  useEffect(() => {
+    if (!sessions.length) {
+      dispatch(getSessions());
+    }
+  }, [sessions]);
+
+  return (
+    <div className={styles.container}>
+      <section className={styles.section}>
+        <h1 className={styles.title}>Status of your session with the psychologist:</h1>
+        {sessions.map((session) => {
+          if (postulantId === session.postulant._id) {
+            return (
+              <div className={styles.sessionDiv} key={session._id}>
+                {session.status === 'assigned' && (
+                  <p>
+                    {postulant.firstName} {postulant.lastName}: Your session is assigned for the
+                    following date: {session.date.replace('T', ' ')}
+                  </p>
+                )}
+                {session.status === 'successful' && (
+                  <p>
+                    {postulant.firstName} {postulant.lastName}: Your session has already passed !
+                    You are enabled to the Interviews section so you can apply for open positions.
+                  </p>
+                )}
+                {session.status === 'cancelled' && (
+                  <p>
+                    {postulant.firstName} {postulant.lastName}: Your session was cancelled.
+                  </p>
+                )}
+              </div>
+            );
+          }
+        })}
+      </section>
+    </div>
+  );
+}
+
+export default Session;
--- src/Routes/postulant.js
+++ src/Routes/postulant.js
@@ -4,6 +4,7 @@ import Profile from 'Components/Postulants/Profile';
 import EditProfile from 'Components/Postulants/Profile/Form';
 import Interviews from 'Components/Postulants/Interviews';
 import Layout from 'Components/Layout';
+import Session from 'Components/Postulants/Sessions';
 
 const PostulantRoutes = () => {
   const { url } = useRouteMatch();
@@ -12,6 +13,7 @@ const PostulantRoutes = () => {
       routes={[
         { name: 'Home', path: '/postulant/home' },
         { name: 'Profile', path: '/postulant/profile' },
+        { name: 'Session', path: '/postulant/session' },
         { name: 'Interviews', path: '/postulant/interviews' },
         { name: 'Logout', path: '/logout' }
       ]}
@@ -21,6 +23,7 @@ const PostulantRoutes = () => {
         <Route exact path={`${url}/profile`} component={Profile} />
         <Route path={`${url}/profile/form`} component={EditProfile} />
         <Route path={`${url}/interviews`} component={Interviews} />
+        <Route path={`${url}/session`} component={Session} />
         {/* <Route path={`${url}/logout`} component={Logout} /> */}
         <Redirect to={`${url}/home`} />
       </Switch>
